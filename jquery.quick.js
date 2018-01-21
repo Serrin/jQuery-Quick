88eeffc78c3cdecc7dcddd6442f0f61e35dd7b1d
Ferenc Czigler <Serrin@users.noreply.github.com>
2018-01-21 18:57:17 +0100

    v1.0.0

--- jquery.quick.js
+++ jquery.quick.js
@@ -1,4 +1,4 @@
 /** Query quick plugin * @version 1.0.0 * @see https://github.com/Serrin/jQuery-Quick */
 "use strict";
 $.fn.quickHide = function () { this.css("display", "none"); };
-$.fn.quickShow = function (value) { this.css("display", value || ""); };
+$.fn.quickShow = function (value) { this.css("display", value || ""); }; 
